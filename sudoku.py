6cffa0ba05c77a72d2da2f3aa18589c17e7b08e9
orik1504 <orik1504@gmail.com>
2021-03-24 22:59:14 +0200

    ✨added sudoku solve
    @RealA10N

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,5 @@
 import random
+import typing
 
 
 class SudokuTable:
@@ -180,9 +181,64 @@ class SmartSuduko(SudokuTable):
             super().set(row, col, temp)
             raise ValueError(f"The value {value} is not allowed in this cell")
 
+    def fill_randomly(self,):
+        """ Solves the Sudoku table! An alias of the solve method. """
+        return self.__solve(1, 1)
+
+    def __next_cell(self, row: int, col: int) -> typing.Tuple[int, int]:
+        """ Recives a cell location and returns the location to the next cell
+        in the order. """
+
+        if col == self._size:
+            return row + 1, 1
+        return row, col + 1
+
+    def __is_at_end(self, row, col) -> bool:
+        """ Returns True only if the last cell is given. """
+        return row > self._size or col > self._size
+
+    def __solve(self,
+                row: int,
+                col: int,
+                ):
+        """ Solves the Sudoku table! Returns True if the soduko is solved and
+        False if it canno't be solved. """
+
+        if self.__is_at_end(row, col):
+            # If we arrived to the end of the table, it means that we found
+            # a "path" to the solution, and we return True!
+            return True
+
+        if self.get(row, col) is not None:
+            # If the current cell is already filled before the solving function,
+            # skip the filling of the current cell
+            next_row, next_col = self.__next_cell(row, col)
+            return self.__solve(next_row, next_col)
+
+        for i in range(1, self._size + 1):
+            # On a 3x3 table i will range from 1 to 9 (including).
+
+            try:
+                # Tries to place the current i in the cell
+                self.set(row, col, i)
+
+            except ValueError:
+                # If canno't place i, we "block the path" and continute
+                # to the next i.
+                continue
+
+            next_row, next_col = self.__next_cell(row, col)
+            path_found = self.__solve(next_row, next_col)
+
+            if path_found:
+                return True
+
+        self.clear(row, col)
+        return False
+
 
 if __name__ == "__main__":
-    table = SudokuTable(3)
+    table = SmartSuduko(3)
     table.fill_randomly()
     print(table)
 
